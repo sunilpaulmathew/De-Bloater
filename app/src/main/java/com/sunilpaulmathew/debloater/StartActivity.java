1bdc5078ba7aee99dcb33751706d6d949888c785
sunilpaulmathew <sunil.kde@gmail.com>
2020-11-02 19:06:19 +0100

    Added a start-up dialogue to acknowledge Magisk
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- app/src/main/java/com/sunilpaulmathew/debloater/StartActivity.java
+++ app/src/main/java/com/sunilpaulmathew/debloater/StartActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.sunilpaulmathew.debloater.utils.Utils;
 
 /*
  * Created by sunilpaulmathew <sunil.kde@gmail.com> on November 1, 2020
@@ -16,6 +19,11 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        AppCompatTextView mMagiskCredit = findViewById(R.id.magisk_credit);
+        if (Utils.isDarkTheme(this)) {
+            mMagiskCredit.setTextColor(Utils.getThemeAccentColor(this));
+        }
 
         new Handler().postDelayed(() -> {
             // Launch MainActivity
